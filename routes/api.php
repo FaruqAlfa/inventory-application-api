fc35927dc059969a683c15cf7796e4b95b700dfd
FaruqAlfa <alfafahmi172@gmail.com>
2024-08-04 21:26:47 +0700

    update api route

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,15 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\InventoryController;
 
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('profile', [UserController::class, 'getProfile']);
-    Route::put('profile', [UserController::class, 'updateProfile']);
+    Route::get('/profile', [UserController::class, 'getProfile']);
+    Route::put('/updateProfile/{id}', [UserController::class, 'updateProfile']);
     
-    Route::post('inventory', [InventoryController::class, 'store']);
-    Route::put('inventory/{id}', [InventoryController::class, 'update']);
-    Route::delete('inventory/{id}', [InventoryController::class, 'destroy']);
-    Route::get('inventory', [InventoryController::class, 'index']);
-    Route::get('inventory/{id}', [InventoryController::class, 'show']);
+    Route::post('/addInventory', [InventoryController::class, 'store']);
+    Route::put('/updateInventory/{id}', [InventoryController::class, 'update']);
+    Route::delete('/deleteInventory/{id}', [InventoryController::class, 'destroy']);
+    Route::get('/inventory', [InventoryController::class, 'index']);
+    Route::get('/showInventoryById/{id}', [InventoryController::class, 'show']);
 });
