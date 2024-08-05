032fef2cbd3ecb572981459690afeacc61101a58
FaruqAlfa <alfafahmi172@gmail.com>
2024-08-05 07:39:07 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
-            'usia' => 20
+            'age' => 20
         ]);
     }
 }
